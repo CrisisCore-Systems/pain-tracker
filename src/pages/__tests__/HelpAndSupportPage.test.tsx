169c9fd0dca1ae6c582a8ed753a281a4346616e8
crisiscore-systems <crisiscore.systems@proton.me>
2026-05-08 13:33:23 -0700

    test: Fix HelpAndSupportPage router context

--- src/pages/__tests__/HelpAndSupportPage.test.tsx
+++ src/pages/__tests__/HelpAndSupportPage.test.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
 import { render, screen } from '../../test/test-utils';
 import HelpAndSupportPage from '../HelpAndSupportPage';
 
 describe('HelpAndSupportPage', () => {
   it('renders support cards', () => {
-    render(<HelpAndSupportPage />);
+    render(<HelpAndSupportPage />, { wrapper: MemoryRouter });
     expect(screen.getByText(/Help & Support/i)).toBeInTheDocument();
   expect(screen.getByRole('heading', { name: /Getting started/i })).toBeInTheDocument();
   expect(screen.getByRole('heading', { name: /FAQs/i })).toBeInTheDocument();
